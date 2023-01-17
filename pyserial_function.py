c16b493617f22adca848800b8a4cd91e5e9d9094
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2023-01-17 17:32:08 +0100

    add pyserial stuff

--- pyserial_function.py
+++ pyserial_function.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+import serial
 
 
 class SerialNode(Node):
@@ -7,8 +8,14 @@ class SerialNode(Node):
         super().__init__('serialnode')
         self.timer = self.create_timer(1, self.timer_callback)
 
+        self.ser = serial.Serial("/dev/ttyUSB0", baudrate=9600)
+        if self.ser.is_open:
+            self.get_logger().info('Serial opened!')
+
     def timer_callback(self):
-        self.get_logger().info('Serial Node is working')
+        while self.ser.is_open:
+            self.ser.write(bytes(b'I am active'))
+            self.get_logger().info('Message sent')
 
 def main(args=None):
     rclpy.init(args=args)
